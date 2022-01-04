0dba79717d5f67d27b778d35d52ff118162fc873
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-01-03 21:36:38 -0300

    add api do google maps

--- src/components/Map/Mapconfig.tsx
+++ src/components/Map/Mapconfig.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+import './styles.css'
 
 export function Mapconfig (){
+
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyD6CVy49q8Oe4_7eEXbN1GMBLdYawCAI4c"
+      })
     return (
-        <div>
-            <h1>ola!</h1>
+        <div className="conteiner">
+           {isLoaded ? (
+            <GoogleMap
+                mapContainerStyle={{width: '100%', height: '100%'}}
+                center={{
+                    lat: -5.528684, 
+                    lng: -47.471169
+                }}
+                zoom={14}
+            >
+            </GoogleMap>
+        ) : <></>}
         </div>
     );
 }
